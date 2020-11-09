0ed66c3a064cf4b13cba5576e94ec9b2f1a558cd
Rohit Raj <rohit.raj@aalto.fi>
2020-11-10 00:16:24 +0200

    FIrst commit for main branch

---
+++ src/components/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+     <!-- <md-tabs router-link>
+        <md-tab md-label="Foo" to="/foo">foo</md-tab>
+        <md-tab md-label="Bar" to="/">Home</md-tab>
+     </md-tabs> -->
+   </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default class Navigation extends Vue { }
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <md-toolbar class="md-primary">
+      <h3 class="md-title">Corona Virus Information</h3>
+    </md-toolbar>
+    <navigation></navigation>
+  </div>
+</template>
+
+<script lang='ts'>
+import Navigation from '@/components/Navigation.vue'
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {
+    Navigation
+  }
+})
+export default class HomePage extends Vue { }
+</script>
