33bdc4549faef8ccaa35acf788e25566f31a7475
Rohit Raj <rohit.raj@aalto.fi>
2020-11-26 19:12:55 +0200

    Completed the 404 page

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-         <router-link to="/homepage">Go to Corona Home</router-link>
-        404- Page not found
+    <div style="margin: 1%;">
+        <md-card md-with-hover>
+            <md-ripple>
+                <md-card-header>
+                    <div class="md-title">404 Page</div>
+                </md-card-header>
+
+                <md-card-content>
+                Looks like this page does not exist. :(
+                </md-card-content>
+                <md-card-actions>
+                    <md-button class="md-raised md-accent" to="/homepage">Go to Homepage
+                    </md-button>
+                </md-card-actions>
+            </md-ripple>
+        </md-card>
     </div>
 </template>
+<style scoped>
+#nav {
+  padding: 30px;
+}
+</style>
